dcb7fd7e6140c6bd9536238d8a0cbcc70b9d9ff2
SophieSchochet <87827703+SophieSchochet@users.noreply.github.com>
2023-12-11 12:01:33 -0600

    added now playing landing page

--- script.js
+++ script.js
@@ -3,25 +3,55 @@ let popUpContainer = document.getElementById("popup-container");
 let bigImg = document.getElementById("clicked-img");
 let expandedOverview = document.getElementById("overview");
 let expandedRating = document.getElementById("rating");
+let releaseDate = document.getElementById("released");
 let bigTitle = document.getElementById("expanded-title");
 let alert = document.getElementById("result-alert")
 let theForm = document.getElementById("search-form");
 let searchTerm = document.getElementById("search-text");
-let clearBtn = document.getElementById("clear-btn");
 
 let baseUrl = "https://api.themoviedb.org/3/search/movie?query=";
 let posterUrl = "https://image.tmdb.org/t/p/original/";
+let defaultUrl = "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1";
 var movies = [];
+
+loadPage();
+
 
 document.getElementById("close-expansion").addEventListener("click",() => {
     popUpContainer.classList.add("hidden");
 });
 
-clearBtn.addEventListener("click",() => {
-    theResults.innerHTML = "";
-    clearBtn.classList.add("hidden");
-    alert.classList.add("hidden");
-});
+theForm.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    let movieName = searchTerm.value;
+    if(movieName != null){
+        theResults.innerHTML = "";
+        getMovies(movieName);
+        alert.innerText = "Results for: " + movieName;
+        searchTerm.value = "";
+    }
+}
+)
+
+function loadPage(){
+    var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        const movieResults = JSON.parse(this.responseText);
+        if(movieResults!= null){
+            movies = movieResults.results;
+            showMovies();
+            }
+        }
+
+};
+xhttp.open("GET", defaultUrl +"&api_key=6d4ebf2688d963156a0d6cb9e7d1e729", true);
+xhttp.setRequestHeader("Authorization",'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZDRlYmYyNjg4ZDk2MzE1NmEwZDZjYjllN2QxZTcyOSIsInN1YiI6IjY1MmQ1NzU3MzU4ZGE3NWI2MWY4Y2UyNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tanpWxjfmzJ89w9gcgEeSx8BGu9OjDCNQs1tRuhj8Zg');
+xhttp.setRequestHeader("Content-type", "application/json");
+xhttp.send();
+}
+
+
 
 
 function showMovies(){
@@ -29,6 +59,7 @@ function showMovies(){
     let posterPath = movies[i].poster_path;
     let movieTitle = movies[i].title;
     let movieRating = "Rating: " + movies[i].vote_average +"/10";
+    let movieDate = movies[i].release_date;
     console.log(movieTitle);
     let newMovie = document.createElement("div");
     newMovie.classList.add("movie");
@@ -47,7 +78,8 @@ function showMovies(){
     newMovie.addEventListener("click",() => {
         bigImg.src = poster.src;
         popUpContainer.classList.remove("hidden");
-        expandedOverview.innerText = "Overview: " + movies[i].overview;
+        expandedOverview.innerText = movies[i].overview;
+        releaseDate.innerText = "Released: " + movieDate;
         expandedRating.innerText = movieRating;
         bigTitle.innerText = movieTitle;
     });
@@ -76,16 +108,3 @@ xhttp.send();
 console.log("entered");
 }
 
-
-theForm.addEventListener("submit", (e) =>{
-    e.preventDefault();
-    let movieName = searchTerm.value;
-    if(movieName != null){
-        getMovies(movieName);
-        clearBtn.classList.remove("hidden");
-        alert.classList.remove("hidden");
-        alert.innerText = "Results for: " + movieName;
-        searchTerm.value = "";
-    }
-}
-) 
